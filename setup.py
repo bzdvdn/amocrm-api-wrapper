96104158cb3389fc460b10586a66b69db42a9f19
bzdvdn <bzdv.dn@gmail.com>
2020-08-28 22:15:28 +0300

    add encoding utf-8 for README

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup, find_packages
 
 
 def read(f):
-    return open(f, "r").read()
+    return open(f, "r", encoding='utf-8').read()
 
 
 setup(
     name="amocrm-api-wrapper",
-    version='0.0.5',
+    version='0.0.6',
     packages=find_packages(exclude=("tests",)),
     install_requires=["requests",],
     description="Amocrm api wrapper v4",
